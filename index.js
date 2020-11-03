34815957a59d47cb908cdf690269392ba3ec9df1
alexxsanya <alexxsanya@gmail.com>
2020-11-04 00:20:43 +0300

    add ref for cron.schedule

--- index.js
+++ index.js
@@ -5,9 +5,22 @@ const mail = require('./includes/mailer')
 app = express();
 
 /*
-    * * * * * - runs every minute
-*/
+https://www.thirdrocktechkno.com/blog/how-to-set-up-a-cron-job-in-nodejs/
 
+┌──────────────── second (optional) (Valid range 0-59)
+| ┌────────────── minute (Valid range: 0-59)
+| | ┌──────────── hour (valid range: 0-23)
+| | | ┌────────── day of the month (Valid range: 1-31)
+| | | | ┌──────── month (Valid range: 1-12)
+| | | | | ┌────── day of the week (valid range: 0-7)
+| | | | | | 
+| | | | | |
+* * * * * *
+*/
+/*
+    * * * * * - runs every minute
+    https://crontab.guru/#10_08_*_2_1 >>> tool to generate schedule parameter
+*/
 // schedule tasks to be run on the server
 cron.schedule('* * * * *', function () {
     console.log('Running Cron Job');
